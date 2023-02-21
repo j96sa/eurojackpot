5027bc9fb806f762ad132fd1c0a6d2451629f07c
Josue Antuan <josue96sa@gmail.com>
2023-02-21 11:31:19 +0100

    ready1.0

---
+++ app.js
@@ -0,0 +1,54 @@
+const $btn = document.querySelector('.btn'),
+$show = document.querySelector('.show');
+
+$btn.addEventListener("click",e=>{
+$show.innerHTML = `<p>${getFullPlay().main}</p> <p>${getFullPlay().second}</p>`
+});
+
+const MAIN_PLAY_NUMBERS = {
+    Zuma:[02,05,15,18,20,24,35,39,41,47,50,52],
+    Paulo:[04,07,13,19,22,26,34,38,44,45,51,53]
+};
+
+const SECOND_PLAY_NUMBERS = {
+    Zuma:[01,04,06,08,11],
+    Paulo:[03,05,07,09,12]
+};
+
+const allMainNumbers = MAIN_PLAY_NUMBERS.Zuma.concat(MAIN_PLAY_NUMBERS.Paulo).sort((a,b)=> a > b ?1 :-1);
+const allSecondNUmbers = SECOND_PLAY_NUMBERS.Zuma.concat(SECOND_PLAY_NUMBERS.Paulo);
+
+const getFullPlay = ()=>{
+    const PLAY = {
+        main:[],
+        second:[]
+    };
+    
+    for (let i = 0; i < 5; i++){
+        const index = Math.floor(Math.random()*allMainNumbers.length);
+        // PLAY.main.push(allMainNumbers[index]);
+        if (PLAY.main.some(e=>e===allMainNumbers[index])){
+            i = i-1;
+        }else{
+            PLAY.main.push(allMainNumbers[index]);
+        };
+    };
+
+    for (let i = 0; i < 2; i++){
+        const index = Math.floor(Math.random()*allSecondNUmbers.length);
+        // PLAY.second.push(allSecondNUmbers[index]);
+        if (PLAY.second.some(e=>e===allSecondNUmbers[index])){
+            i = i-1;
+        }else{
+            PLAY.second.push(allSecondNUmbers[index]);
+        };
+    };
+
+    PLAY.main.sort((a,b)=> a > b ?1 :-1);
+    PLAY.second.sort((a,b)=> a > b ?1 :-1);
+
+    return PLAY;
+};
+
+console.log(getFullPlay());
+
